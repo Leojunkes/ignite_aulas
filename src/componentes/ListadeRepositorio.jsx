29aa549f6bb0d9f7167fbb40e78a520e2f192a0f
leonardo <leojn8@gmail.com>
2021-03-10 01:59:16 -0300

    modulo03

--- src/componentes/ListadeRepositorio.jsx
+++ src/componentes/ListadeRepositorio.jsx
@@ -1,22 +1,28 @@
 import { RepositoryItem } from "./ItemRepositorio";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
 
-const repository = {
-    name:'unform',
-    description:'Forms in React',
-    link:'https://github.com/Leojunkes'
-}
+
 
 export function ListaRepositorio() {
+    const [repositories, setRepositories] = useState([])
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/Leojunkes/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository}/>
+                { repositories.map(repository=>{
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                }) }
+                
+
             </ul>
         </section>
     )
